d10eec3badc2d72a02b8c79f240caf6c555da3ca
Mark Skelton <mark@software13.co.uk>
2015-07-24 22:06:36 +0100

    Version increment

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='indexedzodb',
-      version='0.0.11',
+      version='0.0.12',
       license='MIT',
       description='A simple, indexed model layer over ZODB',
       author='Mark Skelton',
